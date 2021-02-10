e1ba54f97ad017ed3bc15ca021635fcb4f42e140
minkikim98 <minkikim98@gmail.com>
2021-02-10 10:46:49 -0800

    finished whack-a-mole project (except bonus part)

--- mole.js
+++ mole.js
@@ -16,13 +16,11 @@ function getRandomIntInclusive(min, max) {
 }
 
 function popUpRandomMole() {
-  let moleHeads = document.querySelectorAll(".wgs_mole-head");
-  // console.log(moleHeads);
-  let randomMole = getRandomIntInclusive(0, 7);
-  // console.log(randomMole);
-  console.log(moleHeads[randomMole]);
+  let moleHeads = document.querySelectorAll(".wgs_mole-head:not(.wgs_mole-head--whacked)");
+  if (moleHeads.length === 0) return;
+  let randomMole = getRandomIntInclusive(0, moleHeads.length - 1);
   moleHeads[randomMole].classList.remove("wgs_mole-head--hidden");
-  setTimeout(hideMole, 1000, moleHeads[randomMole]);
+  setTimeout(hideMole, 3000, moleHeads[randomMole]);
 }
 
 function hideMole(mole) {
@@ -32,4 +30,11 @@ function hideMole(mole) {
 
 window.addEventListener('DOMContentLoaded', (event) => {
   setTimeout(popUpRandomMole, 0);
+  let moleHeads = document.querySelectorAll(".wgs_mole-head");
+  moleHeads.forEach(head => {
+    head.addEventListener("click", event => {
+      head.classList.add("wgs_mole-head--hidden");
+      head.classList.add("wgs_mole-head--whacked");
+    });
+  });
 });
